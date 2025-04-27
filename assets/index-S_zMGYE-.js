19f2e2170a5652a5d826f09622318ad1cee44d89
grandpierrekevin <grandpierre.kevin@gmail.com>
2025-04-27 22:30:05 +0200

    Updates

---
+++ assets/index-S_zMGYE-.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-DJSkKSkb.js";function s(){return e.jsx("div",{className:"p-6",children:e.jsx("h1",{className:"text-3xl font-bold",children:"Tableau de bord"})})}export{s as default};
