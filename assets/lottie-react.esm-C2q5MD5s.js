27c07e663300342e2aeecfd2d4db18f79a42c9c7
grandpierrekevin <grandpierre.kevin@gmail.com>
2025-04-27 22:39:51 +0200

    Updates

--- assets/lottie-react.esm-C2q5MD5s.js
+++ assets/lottie-react.esm-C2q5MD5s.js
@@ -1,4 +1,4 @@
-import{r as reactExports}from"./index-DJSkKSkb.js";/*! *****************************************************************************
+import{r as reactExports}from"./index-B-UDs_YS.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
